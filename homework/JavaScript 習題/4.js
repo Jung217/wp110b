60afaf3587047697262201f56b84a9b49d7c98b0
Jung217 <99934895+Jung217@users.noreply.github.com>
2022-06-08 16:10:59 +0800

    Add files via upload

---
+++ homework/JavaScript 習題/4.js
@@ -0,0 +1,17 @@
+function filter(lb,hb){
+    let ans=[], a=0;
+    for(lb ; lb<=hb ; lb++)
+    {
+        let check=0;
+        if(lb % 3 == 0) check++;
+        if(lb % 5 == 0) check++;
+        if(lb % 7 == 0) check++;
+        if(check == 0) 
+        {
+            ans[a]=lb;
+            a++;
+        }
+    }
+    return ans;
+}
+console.log(filter(5,15))
